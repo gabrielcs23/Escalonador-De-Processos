2147ced3479bee859dc7ccc1ef45855b4b69836c
Fiuza <gabriel_fiuza@hotmail.com>
2018-05-27 18:49:12 -0300

    main movido para so.py

--- escalonador.py
+++ escalonador.py
@@ -70,29 +70,3 @@ def escalona_mp_suspende(self, qtd_memoria, processosBloqueados, processosBloque
                     return
         # diminui prioridade em 1 para analisar a próxima prioridade
         prioridade -= 1
-
-def main():
-    filaEntrada = inicilizarEntrada('entrada.txt')
-    processosNovos = {'tempoReal': [], 'usuario': []}
-    processosProntos = []
-    processosProntosSuspenso = []
-    processosBloqueados = []
-    processosBloqueadosSuspenso = []
-    processosExecutando = []
-    processosFinalizados = []
-    # TODO some magic
-
-
-# arquivo de entrada deve ter cada parametro do processo separado por VIRGULA + ESPAÇO
-def inicilizarEntrada(nomeArquivo):
-    arquivoEntrada = open(nomeArquivo, 'r')
-    filaEntrada = []
-    for linha in arquivoEntrada:
-        linha.split(', ')
-        novoProcesso = Processo(linha[0], linha[1], linha[2], linha[3], linha[4], linha[5], linha[6], linha[7])
-        filaEntrada.append(novoProcesso)
-    arquivoEntrada.close()
-    return filaEntrada
-
-
-main()  # TODO calling magic
--- so.py
+++ so.py
@@ -1,4 +1,5 @@
 from cpu import CPU
+from processo import Processo
 
 
 class SO:
@@ -64,3 +65,30 @@ class SO:
                     listaProcesso.insert(i, processoT)
                     return
         listaProcesso.insert(i, processoT)
+
+
+def main():
+    filaEntrada = inicilizarEntrada('entrada.txt')
+    processosNovos = {'tempoReal': [], 'usuario': []}
+    processosProntos = []
+    processosProntosSuspenso = []
+    processosBloqueados = []
+    processosBloqueadosSuspenso = []
+    processosExecutando = []
+    processosFinalizados = []
+    # TODO some magic
+
+
+# arquivo de entrada deve ter cada parametro do processo separado por VIRGULA + ESPAÇO
+def inicilizarEntrada(nomeArquivo):
+    arquivoEntrada = open(nomeArquivo, 'r')
+    filaEntrada = []
+    for linha in arquivoEntrada:
+        linha.split(', ')
+        novoProcesso = Processo(linha[0], linha[1], linha[2], linha[3], linha[4], linha[5], linha[6], linha[7])
+        filaEntrada.append(novoProcesso)
+    arquivoEntrada.close()
+    return filaEntrada
+
+
+main()  # TODO calling magic
