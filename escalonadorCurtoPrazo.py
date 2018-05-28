7c83e90f4571997f09523a1a9eea23a859a0f2b2
Felipe <rag.felipe@gmail.com>
2018-05-27 21:40:26 -0300

    Criado escalonador Curto Prazo

--- escalonadorCurtoPrazo.py
+++ escalonadorCurtoPrazo.py
@@ -1,13 +1,43 @@
-
+from gerencia_inout import GerenciaIO
+from so import SO
 
 class escalonadorCurto:
 
+    #função principal que faz uma rodada de escalonador.
+    def rodadaDeEscalonadorCurto(self,listaBloqueado,listaPronto,listaExecutando,listaFinalizados,cpus):
+        self.moveBloqueadoParaExecutando(listaBloqueado,listaPronto)
+        self.alocaProcessosNaCPU(cpus,listaPronto,listaExecutando)
+        self.verificaIO(listaBloqueado,listaExecutando,cpus)
+        self.desalocaProcessosNaCPU(cpus,listaPronto,listaExecutando,listaFinalizados)
+
+
+    #função que verifica se io chegou e move de bloqueado para executando
     def moveBloqueadoParaExecutando(self,listaBloqueado,listaPronto):
-        for processo in listaBloqueado:
-            if processo.qtdModem>0 and GerenciaIO.quantImpressoraDisponivel:
-
-
-
+        for i in range(0,len(listaBloqueado)):
+            if listaBloqueado[i].qtdImpressora>0 and GerenciaIO.quantImpressoraDisponivel()==listaBloqueado[i].qtdImpressora:
+                if GerenciaIO.quantImpressoraDisponivel()==1:
+                    GerenciaIO.impressora_1.ocupado(listaBloqueado[i].id)
+                if GerenciaIO.quantImpressoraDisponivel()==2:
+                    GerenciaIO.impressora_1.ocupado(listaBloqueado[i].id)
+                    GerenciaIO.impressora_2.ocupado(listaBloqueado[i].id)
+                listaPronto.insereProcesso(listaBloqueado[i])
+                listaBloqueado.pop(i)
+            if listaBloqueado[i].qtdCd > 0 and GerenciaIO.quantCDDisponivel() == listaBloqueado[i].qtdCd:
+                if GerenciaIO.quantCDDisponivel() == 1:
+                    GerenciaIO.cd_1.ocupado(listaBloqueado[i].id)
+                if GerenciaIO.quantCDDisponivel() == 2:
+                    GerenciaIO.cd_1.ocupado(listaBloqueado[i].id)
+                    GerenciaIO.cd_2.ocupado(listaBloqueado[i].id)
+                listaPronto.insereProcesso(listaBloqueado[i])
+                listaBloqueado.pop(i)
+            if listaBloqueado[i].qtdScanner > 0 and GerenciaIO.quantScannerDisponivel() == listaBloqueado[i].qtdScanner:
+                GerenciaIO.scanner.ocupado(listaBloqueado[i].id)
+                listaPronto.insereProcesso(listaBloqueado[i])
+                listaBloqueado.pop(i)
+            if listaBloqueado[i].qtdModem > 0 and GerenciaIO.quantModemDisponivel() == listaBloqueado[i].qtdModem:
+                GerenciaIO.Modem.ocupado(listaBloqueado[i].id)
+                listaPronto.insereProcesso(listaBloqueado[i])
+                listaBloqueado.pop(i)
 
     # função para alocar, caso necessário, o processo da lista de memoria para a cpu
     def alocaProcessosNaCPU(self, cpus, listaPronto, listaExecutando):
@@ -39,10 +69,9 @@ class escalonadorCurto:
     #função interrompe com so
     def verificaIO(self, listaBloqueado, listaExecutando, cpus):
         for i in range(0, len(cpus)):
-            if (cpus[i].processo.qtdImpressora or cpus[i].processo.qtdCd or cpus[i].processo.qtdScanner or cpus[
-                i].processo.qtdModem):
+            if (cpus[i].processo.qtdImpressora or cpus[i].processo.qtdCd or cpus[i].processo.qtdScanner or cpus[i].processo.qtdModem):
                 if cpus[i].quantum == 0 and cpus[i].processo != None:
-                    self.insereProcesso(cpus[i].processo, listaBloqueado)
+                    SO.insereProcesso(cpus[i].processo, listaBloqueado)
                     listaExecutando.pop(cpus[i].posicaoLista)
                     cpus[i].processo = None
 
@@ -53,11 +82,11 @@ class escalonadorCurto:
                 # verifica se o processo terminou ou não, se sim, então removo da lista de executando, da cpu e coloca na lista de finalizados
                 if (cpus[i].processo.tempoRestante == 0):
                     cpus[i].processo.tempoFinalizacao = self.tempoSistema
-                    self.listaFinalizados.append(cpus[i].processo)
+                    listaFinalizados.append(cpus[i].processo)
                     listaExecutando.pop(cpus[i].posicaoLista)
                 # senão coloco na lista de prontos e removo da lista de executando
                 else:
-                    self.insereProcesso(cpus[i].processo, listaPronto)
+                    SO.insereProcesso(cpus[i].processo, listaPronto)
                     if (cpus[i].processo.fila == 3):
                         cpus[i].processo.fila = 1
                     else:
